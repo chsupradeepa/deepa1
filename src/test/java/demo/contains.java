46e7b1c5ebc6d095f251f4af0190baac7c01078d
chsupradeepa <144310065+chsupradeepa@users.noreply.github.com>
2023-09-08 11:03:49 +0530

    Update contains.java

--- src/test/java/demo/contains.java
+++ src/test/java/demo/contains.java
@@ -14,7 +14,8 @@ public class contains {
           driver.findElement(By.xpath("//input[contains(@type,'password')]")).sendKeys("manager");
           driver.findElement(By.xpath("//div[contains(text(),'Login')] ")).click();
 	}
-	system.out.println("i am supradeepa");
+
+
 	
 
 }
