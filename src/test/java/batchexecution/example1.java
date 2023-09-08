ca28f54c3de7cfb6215da8da675ef86dd2443440
supra <supra@Supradeepa>
2023-09-08 10:47:16 +0530

    i am deepa

--- src/test/java/batchexecution/example1.java
+++ src/test/java/batchexecution/example1.java
@@ -1,5 +1,6 @@
 package batchexecution;
 
+import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -7,15 +8,18 @@ public class example1 {
   @Test
   public void s1()
   {
-	  Reporter.log(" i am test case1");
+	  Reporter.log(" i am test case1",true);
+Assert.fail();
   }
+  @Test
   public void s2()
   {
-	  Reporter.log(" i am test case2");
+	  Reporter.log(" i am test case2",true);
   }
+  @Test
   public void s3()
   {
-	  Reporter.log("i am test case3");
+	  Reporter.log("i am test case3",true);
 	  
   }
 }
